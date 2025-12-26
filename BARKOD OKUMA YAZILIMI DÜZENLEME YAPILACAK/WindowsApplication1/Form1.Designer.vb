703d88cdb786d6c915f1bea5bdad9a1e4b92db48
sametugrac <sametugrac@outlook.com>
2025-12-26 13:26:05 +0300

    Add files via upload

---
+++ BARKOD OKUMA YAZILIMI DÜZENLEME YAPILACAK/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(697, 244)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 45)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Oto. Tıklama"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(18, 28)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(591, 33)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1650
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button2.Location = New System.Drawing.Point(705, 68)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(126, 52)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Excele Aktar"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.DataGridView1.Location = New System.Drawing.Point(2, 201)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(827, 213)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Column1
+        '
+        Me.Column1.FillWeight = 70.0!
+        Me.Column1.HeaderText = "SIRA NO"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.FillWeight = 160.0!
+        Me.Column2.HeaderText = "OKUNAN DEĞER"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.FillWeight = 70.0!
+        Me.Column3.HeaderText = "OKUNAN DEĞER UZUNLUĞU"
+        Me.Column3.Name = "Column3"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(718, 29)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(85, 33)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.Location = New System.Drawing.Point(291, 166)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 31)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label2.Location = New System.Drawing.Point(-1, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(288, 29)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "OKUNAN SON DEĞER:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label3.Location = New System.Drawing.Point(227, 2)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(162, 24)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "OKUNAN DEĞER"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.Label4.Location = New System.Drawing.Point(681, 5)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(150, 24)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "BARKOD SAYISI"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button3.Location = New System.Drawing.Point(705, 126)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(126, 52)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Excel Klasörü Aç"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(834, 413)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "SAMET UĞRAÇ"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+End Class
---
+++ BARKOD OKUMA YAZILIMI DÜZENLEME YAPILACAK/WindowsApplication1/Form1.vb
@@ -0,0 +1,245 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports Microsoft.Office.Interop
+
+'--------------------------------------------------------------
+' Samet UĞRAÇ 21.05.2019 Tarihinde UYGUR TEKER için yapmıştır.|
+'--------------------------------------------------------------
+
+
+'PROGRAMI OLDUKÇA YALIN BİR BİÇİMDE ANLATTIM
+
+Public Class Form1
+    '--------------------------------------------------------------------------------------------------------------------------------------------------
+    'GLOBAL DEĞİŞKENLERİMİZ HER YERDEN ERİŞEBİLELİM DİYE
+    Dim x
+    Dim a
+    '--------------------------------------------------------------------------------------------------------------------------------------------------
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.RowsDefaultCellStyle.BackColor = Color.Yellow
+
+        TextBox2.TextAlign = HorizontalAlignment.Center
+        TextBox1.Select()
+        TextBox2.Visible = True
+        Timer2.Enabled = True
+        TextBox2.Text = 0
+        'BAZI YERLERDE ---- TEXTBOX1.SELECT ---- GÖREBİLİRSİNİZ BU O KOD BLOĞUNA BAĞLI NESNEYE TIKLANINCA TEXTBOX1.FOCUS'LAN ANLANMINDADIR.
+
+
+    End Sub
+
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        '--------------------------------------------------------------------------------------------------------------------------------------------------
+        'BURASIDA TEXT1 OTOMATİK CLİCK YAPAN BUTONUMUZ 1 SANİYEDE BİR TIKLIYOR BUTONA 
+        Button1.PerformClick()
+        '--------------------------------------------------------------------------------------------------------------------------------------------------
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        '--------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        'FORM İÇİNDE GİZLİ BUTTON VAR BU BUTON ÜZERİNDEN KULLANICI YORULMASIN DİYE  OTOMATİK TIKLAMA İÇİN OLUŞTURULDU
+        If TextBox1.Text = "" Then
+            Timer1.Enabled = False
+            Timer2.Enabled = True
+
+        Else
+            '--------------------------------------------------------------------------------------------------------------------------------------------------
+
+            'TEXTBOX2 SAĞ TARAFTA GÖRÜNEN  KÜÇÜK SARI KUTU BU TEXTBOX SAYAC GÖREVİ GÖRÜYOR HER NE KADAR DOĞRU ÇALIŞMASA'DA
+
+            Timer1.Enabled = True
+            'İŞTE ZURNANIN ZIRT DEDİĞİ YER 2 GÜN BOYUNCA 2 SATIR İÇİN UĞRAŞTIM
+            'BURADA TEXT1 GİRİLEN DEĞER SAYISINI  UZUNLIĞUNU A DEĞİŞKENİNE AKTARIYORUZ
+            'SONRA DATAGRİD EKRANA SATIR EKLERKEN DİYORU Kİ TEXT2(SIRA NO)  TEXT1(BARKOD NUMARASI)  A(SAYAC) SATIRLARINI DATAGRİD EKLİYOR
+            Dim a = TextBox1.Text.Length
+            DataGridView1.Rows.Add(TextBox2.Text, TextBox1.Text, a)
+
+            '--------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+            'GİRİLEN SON DEĞERİ LABEL GÖSTERMEK İÇİN TEXT1 İLE EŞİTLEDİM
+            Label1.Text = TextBox1.Text
+
+
+            '---------------------------------------------------------------------------------------------------------------------------------
+            For x = 0 To DataGridView1.Rows.Count - 2 Step 1  'for döngüsü devreye giriyor
+                DataGridView1.Rows(x).DefaultCellStyle.BackColor = Color.Pink
+                TextBox2.Clear()
+                TextBox1.Clear()
+                TextBox1.Select()
+
+                'DATAGRİD ÜZERİNDE BULUNAN VERİLER DEVAMLI SCOLLBAR YARDIMI İLE AŞAĞI ÇEKİLİYOR GİRİLEN SON VERİ GÖZÜKÜYOR
+                DataGridView1.FirstDisplayedScrollingRowIndex = x
+            Next
+            TextBox2.Text = x
+        End If
+        '---------------------------------------------------------------------------------------------------------------------------------
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+        '---------------------------------------------------------------------------------------------------------------------------------
+        'NE İÇİN YAPTIĞIMI HATIRLAMIYORUM
+        TextBox1.Select()
+        Dim satir As Integer
+        satir = DataGridView1.CurrentRow.Index
+        '---------------------------------------------------------------------------------------------------------------------------------
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        TextBox1.Select()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        '-----------------------------------//////BURASI ÇOK ÖNEMLİ//////-----------------------------------------------
+
+        'excel referans ekleme için
+        'solution exploer üzerinde bulunan Rferances gelip sağ tıklıyoruz
+        'Add referances diyoruz
+        'COM bulunan referandan microsoft interop.excel arıyoruz  
+        'ctrl+f yardımı ile excel yazarsak hemen bulunur
+
+        '---------------------------------------------------------------------------------------------------------------------------------
+
+
+        'C:\\ klasörünün içinde Bilgi_Sayar isimli bir klasör var ise Boolean türündeki değişkenin içine True yazdırılır. Yoksa False yazdırılır.
+        Dim Belgelerim As String
+        Belgelerim = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        Dim klasor_varmi As Boolean = My.Computer.FileSystem.DirectoryExists(Belgelerim + ".\Çıktılar")
+
+        '---------------------------------------------------------------------------------------------------------------------------------
+        If klasor_varmi = False Then
+            Directory.CreateDirectory(Belgelerim + ".\Çıktılar")
+
+        End If
+
+        '---------------------------------------------------------------------------------------------------------------------------------
+
+        'DATAGRİD ÜZERİNE OLUŞTURDUĞUMUZ VERİLERİ EXCELE AKTARMASI İÇİN KULLANDIĞIMIZ DEĞİŞKENLER
+        Dim excel As Excel.Application
+            Dim kitap As Excel.Workbook
+            Dim sayfa As Excel.Worksheet
+            Dim datagrid As DataGridView
+            datagrid = Me.DataGridView1
+            excel = CreateObject("Excel.Application")
+            kitap = excel.Workbooks.Add
+            sayfa = excel.Worksheets(1)
+
+            '---------------------------------------------------------------------------------------------------------------------------------
+
+
+            '-------------------EXCELDEKİ  HEADER KISIMLARI İÇİN AYRILAN BÖLÜM BURADA YAZI TİPİ VE SUTUN GENİŞLİĞİNİ AYARLIYORUZ---------------------------
+
+
+            'exceldeki header bölümü yazı tipleri
+            excel.Application.Cells(1, 1).Font.Size = 15
+            excel.Application.Cells(1, 2).Font.Size = 15
+            excel.Application.Cells(1, 3).Font.Size = 15
+
+            '---------------------------------------------------------------------------------------------------------------------------------
+
+            'Yazı kalınlıkları
+            excel.Application.Cells(1, 1).Font.Bold = True
+            excel.Application.Cells(1, 2).Font.Bold = True
+            excel.Application.Cells(1, 3).Font.Bold = True
+
+            '---------------------------------------------------------------------------------------------------------------------------------
+
+            'Excel Kolon Genişliği
+            excel.Application.Cells(1, 1).ColumnWidth = 10
+            excel.Application.Cells(1, 2).ColumnWidth = 34
+            excel.Application.Cells(1, 3).ColumnWidth = 26
+
+            '---------------------------------------------------------------------------------------------------------------------------------
+
+
+            If klasor_varmi = True Then
+
+
+                '---------------------------------------------------------------------------------------------------------------------------------
+                'FOR DÖNGÜSÜ BURADA DATAGRİD ÜZERİNDEN EXCELE BİLGİ AKTARIYORUZ
+                For yaz = 1 To datagrid.RowCount
+                    sayfa.Cells(yaz, 1).value = datagrid.Rows(yaz - 1).Cells(0).Value
+                    sayfa.Cells(yaz, 2).value = datagrid.Rows(yaz - 1).Cells(1).Value
+                    sayfa.Cells(yaz, 3).value = datagrid.Rows(yaz - 1).Cells(2).Value
+
+
+                    ' x(açıklaması)= Excel üzerinde hangi columns yazıyım diyor
+                    '------------------------------------------------
+                    'sayfa.Cells(yaz,X).value =
+                    '------------------------------------------------
+
+
+                    'x(açıklası)=datagriddeki bilgileri HANGİ HÜCRE BİLGİSİNE göre çekiyim diye soruyor saymaya 0'dan başlıyor dikkat et
+                    '------------------------------------------------
+                    ' datagrid.Rows(yaz - 1).Cells(X).Value
+                    '------------------------------------------------
+                Next
+
+
+
+                'EXCEL HEADER İSİMLERİNİ BURADA GÖSTERTİYORUZ
+                sayfa.Cells(1, 1).value = "Satır No"
+                sayfa.Cells(1, 2).value = "Barkod Numarası"
+                sayfa.Cells(1, 3).value = "Okunan Değer Sayısı"
+
+
+                '---------------------------------------------------------------------------------------------------------------------------------
+                'Excel dosyalarını Gün gün kayıt etmek için burda tarih tanımladık
+                Dim tarih2 As Date
+                tarih2 = DateTime.Now.ToShortDateString()
+
+                '--------------------------------------------------------------------------------------------------------------------------------------------------
+
+                'burada  belgelerim klasörü içinde  çıktılar klasörü kurulacak onun için sev edecek program
+                kitap.SaveAs(".\Çıktılar\" + tarih2 + ".xlsx")
+
+                excel.Quit()
+                MsgBox("Belgerim\Çıktılar\ Klasörüne Excel Formatında Aktarıldı")
+            End If
+
+
+        TextBox1.Select()
+
+    End Sub
+
+    Private Sub TextBox2_Click(sender As Object, e As EventArgs) Handles TextBox2.Click
+        TextBox1.Select()
+
+    End Sub
+
+    Private Sub DataGridView1_Click(sender As Object, e As EventArgs) Handles DataGridView1.Click
+        TextBox1.Select()
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox1.Select()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Process.Start("C:\Users\SAMET\Documents\Çıktılar\")
+
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
+
+'----------------------------------------------------------------------------------------...more
